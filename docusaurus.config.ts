f115982ec026b23c7d9052b65498954bb1634585
Eucatastrophe <108638101+makrcat@users.noreply.github.com>
2025-07-03 22:04:56 -0700

    art :)

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -6,7 +6,7 @@ import type * as Preset from '@docusaurus/preset-classic';
 
 const config: Config = {
   title: 'sciolyskillz',
-  tagline: 'B)',
+  tagline: 'meow meow meow meow meow. B)',
   favicon: 'img/favicon.ico',
 
   // Future flags, see https://docusaurus.io/docs/api/docusaurus-config#future
