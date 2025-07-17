564ecb64b7afeae880cbabc9b9c4f7338613860d
Eucatastrophe <108638101+makrcat@users.noreply.github.com>
2025-07-17 16:04:49 -0700

    tigjkbmkl,fgm,.bv

--- src/components/SearchDropdown.tsx
+++ src/components/SearchDropdown.tsx
@@ -1,120 +1,129 @@
 import React, { useState, useRef, useEffect } from "react";
+import { connectSearchBox } from "react-instantsearch-dom";
 
-export default function SearchDropdown() {
-  const [dropdownOpen, setDropdownOpen] = useState(false);
-  const [division, setDivision] = useState("B");
-  const dropdownRef = useRef(null);
+const CustomSearchBox = connectSearchBox(SearchDropdown);
+export default CustomSearchBox;
 
-  // Close dropdown if clicked outside
-  useEffect(() => {
-    function handleClickOutside(event) {
-      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-        setDropdownOpen(false);
-      }
-    }
-    document.addEventListener("mousedown", handleClickOutside);
-    return () => document.removeEventListener("mousedown", handleClickOutside);
-  }, []);
 
-  return (
-    <form className="max-w-lg mx-auto bg-gradient-to-r from-teal-500 to-blue-600 p-4 rounded-lg">
-      <div className="flex">
-        <label
-          htmlFor="search-dropdown"
-          className="mb-2 text-sm font-medium text-white sr-only"
-        >
-          Select Division
-        </label>
+function SearchDropdown({ currentRefinement, refine }) {
+    const [dropdownOpen, setDropdownOpen] = useState(false);
+    const [division, setDivision] = useState("B");
+    const dropdownRef = useRef(null);
 
-        {/* Dropdown Button */}
-        <div className="relative" ref={dropdownRef}>
-          <button
-            type="button"
-            onClick={() => setDropdownOpen(!dropdownOpen)}
-            id="dropdown-button"
-            aria-haspopup="listbox"
-            aria-expanded={dropdownOpen}
-            className="shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none force-border focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
-          >
-            {division}{" "}
-            <svg
-              className="w-2.5 h-2.5 ms-2.5"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 10 6"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m1 1 4 4 4-4"
-              />
-            </svg>
-          </button>
+    // Close dropdown if clicked outside
+    useEffect(() => {
+        function handleClickOutside(event) {
+            if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+                setDropdownOpen(false);
+            }
+        }
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => document.removeEventListener("mousedown", handleClickOutside);
+    }, []);
 
-          {/* Dropdown Menu */}
-          {dropdownOpen && (
-            <div className="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 mt-1 dark:bg-gray-700">
-              <ul
-                className="py-2 text-sm text-gray-700 dark:text-gray-200"
-                role="listbox"
-                aria-labelledby="dropdown-button"
-              >
-                {["B", "C"].map((option) => (
-                  <li key={option}>
+    return (
+        <form
+        className="max-w-lg mx-auto bg-gradient-to-r from-teal-500 to-blue-600 p-4 rounded-lg" 
+        onSubmit={(e) => e.preventDefault()}>
+            <div className="flex">
+                <label
+                    htmlFor="search-dropdown"
+                    className="mb-2 text-sm font-medium text-white sr-only"
+                >
+                    Select Division
+                </label>
+
+                {/* Dropdown Button */}
+                <div className="relative" ref={dropdownRef}>
                     <button
-                      type="button"
-                      onClick={() => {
-                        setDivision(option);
-                        setDropdownOpen(false);
-                      }}
-                      className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
-                      role="option"
-                      aria-selected={division === option}
+                        type="button"
+                        onClick={() => setDropdownOpen(!dropdownOpen)}
+                        id="dropdown-button"
+                        aria-haspopup="listbox"
+                        aria-expanded={dropdownOpen}
+                        className="shrink-0 z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-gray-900 bg-gray-100 border border-gray-300 rounded-s-lg hover:bg-gray-200 focus:ring-4 focus:outline-none force-border focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600"
                     >
-                      {option}
+                        {division}{" "}
+                        <svg
+                            className="w-2.5 h-2.5 ms-2.5"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 10 6"
+                        >
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="m1 1 4 4 4-4"
+                            />
+                        </svg>
                     </button>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          )}
-        </div>
 
-        {/* Search input with button */}
-        <div className="relative w-full">
-          <input
-            type="search"
-            id="search-dropdown"
-            className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
-            placeholder="Search by ID, text, or anything else"
-            required
-          />
-          <button
-            type="submit"
-            className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-          >
-            <svg
-              className="w-4 h-4"
-              aria-hidden="true"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="currentColor"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
-              />
-            </svg>
-            <span className="sr-only">Search</span>
-          </button>
-        </div>
-      </div>
-    </form>
-  );
+                    {/* Dropdown Menu */}
+                    {dropdownOpen && (
+                        <div className="absolute z-10 bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 mt-1 dark:bg-gray-700">
+                            <ul
+                                className="py-2 text-sm text-gray-700 dark:text-gray-200"
+                                role="listbox"
+                                aria-labelledby="dropdown-button"
+                            >
+                                {["B", "C"].map((option) => (
+                                    <li key={option}>
+                                        <button
+                                            type="button"
+                                            onClick={() => {
+                                                setDivision(option);
+                                                setDropdownOpen(false);
+                                            }}
+                                            className="inline-flex w-full px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white"
+                                            role="option"
+                                            aria-selected={division === option}
+                                        >
+                                            {option}
+                                        </button>
+                                    </li>
+                                ))}
+                            </ul>
+                        </div>
+                    )}
+                </div>
+
+                {/* Search input with button */}
+                <div className="relative w-full">
+                    <input
+                        type="search"
+                        id="search-dropdown"
+                        value={currentRefinement}
+                        onChange={(e) => refine(e.currentTarget.value)}
+                        className="block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 rounded-e-lg border-s-gray-50 border-s-2 border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-s-gray-700  dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500"
+                        placeholder="Search by ID, text, or anything else"
+                        required
+                    />
+                    <button
+                        type="submit"
+                        className="absolute top-0 end-0 p-2.5 text-sm font-medium h-full text-white bg-blue-700 rounded-e-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+                    >
+                        <svg
+                            className="w-4 h-4"
+                            aria-hidden="true"
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 20 20"
+                        >
+                            <path
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"
+                            />
+                        </svg>
+                        <span className="sr-only">Search</span>
+                    </button>
+                </div>
+            </div>
+        </form>
+    );
 }
--- src/pages/problemBank.tsx
+++ src/pages/problemBank.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { algoliasearch } from 'algoliasearch';
 import Layout from '@theme/Layout';
-import SearchDropdown  from '../components/SearchDropdown';
+import SearchDropdown from '../components/SearchDropdown';
 
 import '../css/custom.css';
 
@@ -17,6 +17,7 @@ import {
 } from 'react-instantsearch-dom';
 
 
+
 const appID = "9UDFND4IRA";
 const searchAPIkey = "b9d0863d3b39bf08b9180c5937d38ee8";
 
@@ -25,13 +26,22 @@ const searchClient = algoliasearch(
     searchAPIkey
 );
 
+
+
 function Hit({ hit }) {
     return (
-        <div>
-            <h3>{hit.question}</h3>
-            <p>System: {hit.system}</p>
-            <p>Division: {hit.division}</p>
-            {/* Customize how you want to display each question */}
+        <div className=" bg-white dark:bg-gray-800 rounded-xl shadow-md p-4 border border-gray-200 dark:border-gray-700 hover:shadow-lg transition-shadow">
+            <h3 className="text-lg font-semibold text-gray-800 dark:text-white mb-2">
+                {hit.question}
+            </h3>
+            <div className="text-sm text-gray-600 dark:text-gray-300 space-y-1">
+                <p>
+                    <span className="font-medium text-gray-700 dark:text-gray-200">System:</span> {hit.system}
+                </p>
+                <p>
+                    <span className="font-medium text-gray-700 dark:text-gray-200">Division:</span> {hit.division}
+                </p>
+            </div>
         </div>
     );
 }
@@ -41,34 +51,23 @@ export default function App() {
     return (
         <Layout>
             <InstantSearch indexName="sciolyskillz" searchClient={searchClient}>
-                <div className="max-w-4xl mx-auto p-6">
 
+                <div className="max-w mx-auto">
+                    <div style={{ "height": "50vh" }}
+                        className=" bg-[rgb(48,132,84)] text-white pt-[15vh]" >
 
-                    <h1 className="text-3xl font-bold mb-4 text-center">Question Search</h1>
+                            <h1 className="text-3xl font-bold mb-4 text-center">Question Search</h1>
 
-
-                    
-                    <SearchDropdown/>
-                    <SearchBox
-                        className="mb-4"
-                        translations={{ placeholder: 'Search questions...' }}
-                        submitIconComponent={() => <span>🔍</span>}
-                        resetIconComponent={() => <span>❌</span>}
-                    />
-
-
-
+                            <SearchDropdown />
+                    </div>
 
                     <Hits hitComponent={Hit} />
-
                     <div className="mt-6 flex justify-center">
                         <Pagination />
                     </div>
 
                     <Configure hitsPerPage={10} />
                 </div>
-
-
             </InstantSearch>
         </Layout>
     );
