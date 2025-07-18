a2790bf9c4b99e4aefb39f93b85c2e55c340c8e4
Eucatastrophe <108638101+makrcat@users.noreply.github.com>
2025-07-18 16:47:09 -0700

    aughuhuehudsjkvklx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Link from '@docusaurus/Link';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import Layout from '@theme/Layout';
 
-
+import LikeAndSubscribe from '../components/HomepageFeatures/LikeAndSubscribe';
 import styles from './index.module.css';
 
 
@@ -27,45 +27,6 @@ function AAAAAAA() {
   )
 }
 
-function LikeAndSubscribe() {
-  return (
-    <div className="w-full lg:py-16 lg:px-6">
-
-      <div className="w-full sm:text-center">
-        <h2 className="text-gray-900 sm:text-4xl">Join the mailing list</h2>
-        <p className="mb-8 mx-auto max-w-2xl font-regular text-gray-500 sm:text-xl dark:text-gray-400">Get notified when stuff happens! Like releases, updates, features, etc.</p>
-
-        <div className="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
-          <div className="relative w-full">
-
-
-            <label className="hidden mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">Email address</label>
-
-
-            <div className="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
-              <svg className="w-5 h-5 text-gray-500 " fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"></path><path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"></path></svg>
-            </div>
-
-
-            <input className="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg" placeholder="Enter your email" type="email" id="email" />
-          </div>
-
-
-          <div>
-            <button type="submit" className="text-white font-bold py-3 px-5 w-full text-sm text-center bg-[rgba(47,133,85,255)] rounded-lg border cursor-pointer border-primary-600 sm:rounded-none sm:rounded-r-lg" style={{ "border": "1px solid rgba(47,133,85,255)", "borderLeft": "none" }}>Thanks!</button>
-          </div>
-
-
-        </div>
-
-
-        <div className="mx-auto max-w-screen-sm text-sm text-left text-gray-500">Data exists lol. <a href="#" className="font-medium text-primary-600 dark:text-primary-500 hover:underline">There is no Privacy Policy</a>.</div>
-
-      </div>
-    </div>
-
-  )
-}
 
 
 function HomepageHeader() {
@@ -109,12 +70,12 @@ export default function Home(): ReactNode {
       <HomepageHeader />
       <main>
 
-        <div className="mt-10 text-center" style={{ fontFamily: 'Poppins', }}>
-          <h1>Don't stress, just start.</h1>
+        <div className={styles.AUGHJHHHHH}>
+          Don't stress, just start.
         </div>
 
 
-        <div className="mx-[20vh] m-6 flex flex-col gap-12 p-2">
+        <div className="mx-[25vh] m-6 flex flex-col gap-12 p-2">
 
 
           <div className="
@@ -125,12 +86,12 @@ export default function Home(): ReactNode {
             ">
 
 
-            <div className="flex-1">
-              bro i cant ui design what the fudsioxfjdklvaxcklvfdkgjsklfjgjcbv
+            <div>
+              bro i cant ui design what
             </div>
 
 
-            <div className="flex-1">
+            <div>
               <h2>Docs Guide</h2>
               <p>Yay docs! Wow! Wonderful! AAAAAAAAA I hope docs will be useful</p>
             </div>
